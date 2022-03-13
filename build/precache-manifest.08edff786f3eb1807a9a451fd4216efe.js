d1e1d77b9d334616aa444cf339499a3dbe9e93cc
Varsha-Shivhare <50067937+Varsha-Shivhare@users.noreply.github.com>
2022-03-14 00:21:00 +0530

    added

---
+++ build/precache-manifest.08edff786f3eb1807a9a451fd4216efe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "00c7609036d321e5da41",
+    "url": "/Recipe.github.io/static/css/main.429f7d80.chunk.css"
+  },
+  {
+    "revision": "00c7609036d321e5da41",
+    "url": "/Recipe.github.io/static/js/main.448b4807.chunk.js"
+  },
+  {
+    "revision": "46254570fe41ccb5887d",
+    "url": "/Recipe.github.io/static/js/runtime~main.d2212a9e.js"
+  },
+  {
+    "revision": "c2f1e2d47c6d98c45ffd",
+    "url": "/Recipe.github.io/static/js/2.3668dfba.chunk.js"
+  },
+  {
+    "revision": "1b49cb15319fe3c6c18854668d42657c",
+    "url": "/Recipe.github.io/static/media/Idli.1b49cb15.jpg"
+  },
+  {
+    "revision": "7bc7b0c17d7b0a46fba272f9408944aa",
+    "url": "/Recipe.github.io/static/media/dosa.7bc7b0c1.jpg"
+  },
+  {
+    "revision": "1fdd645526095241a142d3fe6824bc22",
+    "url": "/Recipe.github.io/static/media/sandwich.1fdd6455.png"
+  },
+  {
+    "revision": "cba7b27c185f889763366fdb17e5da62",
+    "url": "/Recipe.github.io/static/media/franky.cba7b27c.jpg"
+  },
+  {
+    "revision": "5d41ae413003408c2b0f71dc15dd99a9",
+    "url": "/Recipe.github.io/static/media/burger.5d41ae41.jpg"
+  },
+  {
+    "revision": "359aa5741a8784a9c68f522424e3e0ea",
+    "url": "/Recipe.github.io/static/media/pasta.359aa574.jpg"
+  },
+  {
+    "revision": "306d3ea38d83f24ea7020fba56efc1ff",
+    "url": "/Recipe.github.io/static/media/maggie.306d3ea3.png"
+  },
+  {
+    "revision": "2fc12e8e5b01a0bba423dd74ed265897",
+    "url": "/Recipe.github.io/static/media/momos.2fc12e8e.jpg"
+  },
+  {
+    "revision": "9199331c00c34a97e3b79f2bda72dc9e",
+    "url": "/Recipe.github.io/static/media/tea.9199331c.jpg"
+  },
+  {
+    "revision": "1b4d3f150281ca8a82d9bad50d002d69",
+    "url": "/Recipe.github.io/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6549308cb06255e1e1c0a738594bb27.js"
+  "/Recipe.github.io/precache-manifest.08edff786f3eb1807a9a451fd4216efe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Recipe.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
