3a362f09b7454c6f726847cbff6e6a4fe17ca31f
Varsha-Shivhare <50067937+Varsha-Shivhare@users.noreply.github.com>
2023-12-27 12:49:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import Restaurant from './Components/Receipe/Restaurant';
-import Cart from './Components/Receipe/Cart/Cart';
+// import Cart from './Components/Receipe/Cart/Cart';
 
 
 function App() {
   return (
     <div>
       <Restaurant />
-     <Cart />
+     {/* <Cart /> */}
     </div>
   );
 }
 
 export default App;
-
-
-
-//
-//import Cart from './Components/Cart';
-//import Learn from './css-pract/Learn';
-//import Input from './Components/ToDoList/Input';
-//import Calculator from './Components/Calculator/Calculator';
-//<Cart />
-//<Learn />
-//<Input />
-//<Calculator />
-//import Bulb from './Components/Bulb/Bulb';
-// <Bulb />
