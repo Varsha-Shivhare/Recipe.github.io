b23eacb20b55a347550ca7e4450849370ff91703
Varsha-Shivhare <50067937+Varsha-Shivhare@users.noreply.github.com>
2023-12-27 12:48:08 +0530

    Update Navbar.js

--- src/Components/Receipe/Navbar.js
+++ src/Components/Receipe/Navbar.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Cart/Cart.css';
-import MenuAPI from './MenuAPI';
-import MenuData from './MenuData';
 
 const Navbar = ({filterItem, navItem, setSearchText}) => {
 
